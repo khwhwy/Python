1ed5b6f1f32427d6b156e6b345f93f6091e07125
khwhwy <lovenosh.6699@gmail.com>
2025-03-07 08:48:41 +0500

    Update facebook id hack.py

--- facebook id hack.py
+++ facebook id hack.py
@@ -1,5 +1,5 @@
 # Author-Kingslayer
-# Email-kingslayer8509@gmail.com
+# Email-khanmani403@email.com
 # you need to create a file password.txt which contains all possible passwords
 import requests
 import threading
